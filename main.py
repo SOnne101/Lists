f807aeff6599330e998dcc6a9503157568819746
SOnne101 <bendigsunne@gmail.com>
2020-02-24 18:43:44 +0100

    Can now select random point from data-point

--- main.py
+++ main.py
@@ -11,21 +11,26 @@ img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert to gray scale
 img_g = cv2.GaussianBlur(img_g, (5,5), cv2.BORDER_DEFAULT) # Blure images for less details
 ret, img_g = cv2.threshold(img_g, 125, 255, cv2.THRESH_BINARY) # Binarization threshold
 
+print('shape[0] is {}, shape[1] is {}'.format(img_g.shape[0], img_g.shape[1]))
+
 # Creating dataset
 
 data = [] # Array for holding our pixel coords
 
-for cols in range(img_g.shape[0]): 
-    for rows in range(img_g.shape[1]):
-        if img_g.item(cols, rows) == 0:
-            data.append((cols, rows))
+for rows in range(img_g.shape[0]): # Loop throguh the rows
+    for cols in range(img_g.shape[1]): # Loop through the cols
+        if img_g.item(rows, cols) == 0: # Chech if the pixel is black
+            data.append((rows, cols)) # Save it to the data array
 
 for i in range(100):
     index = random.randint(0, len(data))
-    img_g[data[index]] = 127
-    cv2.imshow('image',img_g) # Show binary images
+    temp =  img[data[index]]
+    temp = tuple(temp)
+    img[data[index]] = (0, 0, 255)
+    cv2.imshow('image',img) # Show binary images
+    cv2.imshow('gray',img_g) # Show binary images
     cv2.waitKey(100) # Wait for key press
-    img_g[data[index]] == 0
+    img[data[index]] = temp
 
 # cv2.imshow('image0',img) # Show original images
 # cv2.imshow('image',img_g) # Show binary images
